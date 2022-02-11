2a12cedb592a71e9e233840c8e4e1491eb20dd87
IT21212536 <99463746+IT21212536@users.noreply.github.com>
2022-02-11 06:30:44 +0000

    IT21212536

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,16 @@
-/* Exercise 1 - Calculations
+#include<stdio.h>
+int main(){
 
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+  float mark1,mark2,avarage;
 
-#include <stdio.h>
+  printf("input mark 1:");
+  scanf("%f", &mark1);
+  printf("input mark 2 :");
+  scanf("%f", &mark2);
 
-int main() {
-  
+  avarage = (mark1 + mark2)/2;
+
+  printf("avarage is : %.2f",avarage);
+
   return 0;
 }
-
--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,19 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+
+  int sum = 0, num, i;
+
+  printf("enter a number : ");
+  scanf("%d",&num);
+
+  for(i = 1; i <= num; i++)+{
+    sum = sum + i;
+
+  }
   
+  printf ("sum is : %d",sum);
+
   return 0;
 }
 
